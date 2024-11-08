b01525ba876500a2035587817da3f339c6a54765
Brian Sam-Bodden <bsb@redis.com>
2024-11-08 16:01:38 -0700

    Initial empty commit

---
+++ src/main/java/com/redis/temporal/RomsDatesAndTimesApplication.java
@@ -0,0 +1,13 @@
+package com.redis.temporal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomsDatesAndTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomsDatesAndTimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/temporal/RomsDatesAndTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.temporal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomsDatesAndTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
